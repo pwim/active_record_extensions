2f367a6b351da6f74418767f2af0411df40515d2
Paul McMahon <paul@mobalean.com>
2011-01-10 16:15:33 +0900

    Add Railtie

--- lib/active_resource_extensions.rb
+++ lib/active_resource_extensions.rb
@@ -1,3 +1,11 @@
+require 'active_resource_extensions/railtie'
 module ActiveResourceExtensions
   autoload :GracefulErrors, "active_resource_extensions/graceful_errors"
+  def inherited(subclass)
+    subclass.class_eval do
+      class << self
+        include ActiveResourceExtensions::GracefulErrors
+      end
+    end
+  end
 end
