8b26d82545b66130f1d440265642188d04c91dc5
Paul McMahon <paul@mobalean.com>
2011-01-11 14:37:07 +0900

    Move methods to spec helper

--- spec/active_resource_extensions/graceful_errors_spec.rb
+++ spec/active_resource_extensions/graceful_errors_spec.rb
@@ -3,40 +3,25 @@ require 'active_resource'
 
 describe ActiveResourceExtensions::GracefulErrors do
   before do
-    mock = Class.new(ActiveResource::Base)
-    mock.class_eval do
-      class << self
-        attr_accessor :exception
-
-        def find_every(option)
-          raise exception
-        end
-      end
-    end
-    @resource = Class.new(mock)
-    @resource.class_eval do
-      class << self
-        include ActiveResourceExtensions::GracefulErrors
-      end
-    end
+    @resource = mock_resource(ActiveResourceExtensions::GracefulErrors)
     Rails.logger = mock("logger")
     Rails.logger.stub!(:fatal)
   end
 
   describe "Errno::ECONNREFUSED thrown" do
-    before { @resource.exception = Errno::ECONNREFUSED }
+    before { @resource.proc = lambda { raise Errno::ECONNREFUSED } }
     it("first should return nil") { @resource.first.should be_nil }
     it("all should return empty array") { @resource.all.should be_empty }
   end
 
   describe "ActiveResource::TimeoutError thrown" do
-    before { @resource.exception = ActiveResource::TimeoutError.new("message") }
+    before { @resource.proc = lambda { raise ActiveResource::TimeoutError.new("message") } }
     it("first should return nil") { @resource.first.should be_nil }
     it("all should return empty array") { @resource.all.should be_empty }
   end
 
   describe "arbitrary exception thrown" do
-    before { @resource.exception = "arbitrary" }
+    before { @resource.proc = lambda { raise "arbitrary" } }
     it("first should raise exception") { lambda { @resource.first }.should raise_error }
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,22 @@
 $:.unshift File.expand_path('../../lib', __FILE__)
 require 'active_resource_extensions'
+require 'active_resource'
+
+def mock_resource(mod)
+  mock = Class.new(ActiveResource::Base)
+  mock.class_eval do
+    class << self
+      attr_accessor :proc
+      def find_every(option)
+        proc.call
+      end
+    end
+  end
+  resource = Class.new(mock)
+  resource.class_eval(<<-EOD)
+    class << self
+      include #{mod}
+    end
+  EOD
+  resource
+end
