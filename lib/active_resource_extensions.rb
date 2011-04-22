8a74fc6983a8f27c4f7e0442eda849eb5a64fbfb
Paul McMahon <paul@mobalean.com>
2011-04-22 17:06:01 +0900

    Probably not the best idea to always be including these extensions...

--- lib/active_resource_extensions.rb
+++ lib/active_resource_extensions.rb
@@ -1,15 +1,4 @@
-require 'active_resource_extensions/railtie'
 module ActiveResourceExtensions
   autoload :GracefulErrors, "active_resource_extensions/graceful_errors"
   autoload :Caching, "active_resource_extensions/caching"
-  def inherited(subclass)
-    subclass.class_eval do
-      class << self
-        # Caching is included before GracefulErrors to avoid caching error
-        # results
-        include ActiveResourceExtensions::Caching
-        include ActiveResourceExtensions::GracefulErrors
-      end
-    end
-  end
 end
